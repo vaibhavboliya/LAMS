bad90e1520c447584a02ceec91ca2015bb3acbf6
vaibhavboliya <53927146+vaibhavboliya@users.noreply.github.com>
2020-12-04 01:18:34 +0530

    styling of  login screens
    styling of  login screens

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,12 +20,18 @@
             }
         </style>
     </head>
-    <body>
+    <style>
+        #home{
+            padding:1rem 1.5rem;
+            border-radius: 0.5rem;
+        }
+    </style>
+    <body style="background-color:#4a5568 ; color:#edf2f7">
         <div>
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
-                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
+                        <a  class="text-light" href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
                     @else
                         <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
 
@@ -39,6 +45,7 @@
         <div class="text-center">
             <br><br><br><br><br><br><br><br><br>
             <h1>Leave and Attendance Management System</h1>
+            <button type="button" id="home" class="btn btn-primary">Home</button>
         </div>
     </body>
 </html>
