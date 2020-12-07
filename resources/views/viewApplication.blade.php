c0a591abc5eba6d7c4a14edc68358ca4b837fb44
NIKHILG2019 <52930019+NIKHILG2019@users.noreply.github.com>
2020-12-07 17:40:21 +0530

    minor bug fix

--- resources/views/viewApplication.blade.php
+++ resources/views/viewApplication.blade.php
@@ -28,9 +28,6 @@
                 <li class="nav-item">
                     <a class="nav-link active" href={{route('viewLeave')}}>Leave Applications</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href=#>Profile</a>
-                </li>
             </ul>
             <ul class="nav navbar-nav ml-auto">
                 <li class="nav-item"><a class="text-decoration-none text-light" href="/logout">Logout</a></li>
