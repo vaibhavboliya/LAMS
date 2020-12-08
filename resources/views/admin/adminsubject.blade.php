335586cddebed8eec5be3ac1e3f9e9ae7f326289
vaibhavboliya <53927146+vaibhavboliya@users.noreply.github.com>
2020-12-08 12:48:11 +0530

    update

--- resources/views/admin/adminsubject.blade.php
+++ resources/views/admin/adminsubject.blade.php
@@ -12,7 +12,7 @@
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
 
 </head>
-<body style="background-color: #efefef">
+<body >
 
     <div id="wrapper" >
 
